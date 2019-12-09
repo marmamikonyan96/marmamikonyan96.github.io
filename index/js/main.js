8749531c97eecd5fe7f5156c1194a5cd7abf519f
marmamikonyan96 <58704193+marmamikonyan96@users.noreply.github.com>
2019-12-10 02:35:53 +0400

    Responsive design

---
+++ index/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        responsiveClass:true,
+        nav:true,
+        navText:['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+        }
+    })
+});
