88a2c4e2d7eeb797d30f99a4e34c5d13aa4192c2
marmamikonyan96 <58704193+marmamikonyan96@users.noreply.github.com>
2020-10-29 20:28:21 +0400

    update

--- task/js/index.js
+++ task/js/index.js
@@ -10,7 +10,20 @@ if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){
 }  
 
 
-$("#toggle").click(function () {
-  $(this).toggleClass("active");
-  $("#overlay").toggleClass("open");
-});
+function myFunction() {
+  document.getElementById("dropdown").classList.toggle("show");
+}
+
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
