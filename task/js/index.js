ca75747100f79da8d76bc5dde7e0d77ec1bcdd85
marmamikonyan96 <58704193+marmamikonyan96@users.noreply.github.com>
2020-10-30 12:16:10 +0400

    update

--- task/js/index.js
+++ task/js/index.js
@@ -4,7 +4,19 @@ var x=document.myform.email.value;
 var atposition=x.indexOf("@");  
 var dotposition=x.lastIndexOf(".");  
 if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
-  alert("Please enter a valid e-mail address \n atpostion:"+atposition+"\n dotposition:"+dotposition);  
+  alert("Please enter a valid e-mail address ");  
   return false;  
   }  
 }  
+
+
+
+$(document).ready(function() {
+  $('.open-menu').on('click', function() {
+     $('.overlay').addClass('open');
+  });
+ 
+  $('.close-menu').on('click', function() {
+    $('.overlay').removeClass('open');
+  });
+});
