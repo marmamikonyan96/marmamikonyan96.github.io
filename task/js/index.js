4b04f699873f42ec14575380680553144b22791f
marmamikonyan96 <58704193+marmamikonyan96@users.noreply.github.com>
2020-10-30 13:23:52 +0400

    update

--- task/js/index.js
+++ task/js/index.js
@@ -1,22 +1,25 @@
-function validateemail()  
-{  
-var x=document.myform.email.value;  
-var atposition=x.indexOf("@");  
-var dotposition=x.lastIndexOf(".");  
-if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
-  alert("Please enter a valid e-mail address ");  
-  return false;  
-  }  
-}  
+/** @format */
 
+function validateemail() {
+  var x = document.myform.email.value;
+  var atposition = x.indexOf("@");
+  var dotposition = x.lastIndexOf(".");
+  if (
+    atposition < 1 ||
+    dotposition < atposition + 2 ||
+    dotposition + 2 >= x.length
+  ) {
+    alert("Please enter a valid e-mail address ");
+    return false;
+  }
+}
 
-
-$(document).ready(function() {
-  $('.open-menu').on('click', function() {
-     $('.overlay').addClass('open');
+$(document).ready(function () {
+  $(".open-menu").on("click", function () {
+    $(".overlay").addClass("open");
   });
- 
-  $('.close-menu').on('click', function() {
-    $('.overlay').removeClass('open');
+
+  $(".close-menu").on("click", function () {
+    $(".overlay").removeClass("open");
   });
 });
