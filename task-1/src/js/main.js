1ccabf5e1bd7766981ddec7247f2aac74b5a4ffb
marmamikonyan96 <58704193+marmamikonyan96@users.noreply.github.com>
2020-11-08 23:01:36 +0400

    start

---
+++ task-1/src/js/main.js
@@ -0,0 +1,21 @@
+/** @format */
+
+import "./vendor/focus-visible.min.js";
+import "./_vars";
+
+const slider = document.querySelector(".swiper-container");
+
+let mySwiper = new Swiper(slider, {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
