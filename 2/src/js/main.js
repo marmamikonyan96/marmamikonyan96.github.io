0150eb4a50289bbbdd99185d474417a0efb373c7
marmamikonyan96 <58704193+marmamikonyan96@users.noreply.github.com>
2020-11-09 11:56:50 +0400

    ok

--- 2/src/js/main.js
+++ 2/src/js/main.js
@@ -1,18 +1,1 @@
 /** @format */
-const slider = document.querySelector(".swiper-container");
-
-let mySwiper = new Swiper(slider, {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    type: "String",
-  },
-  centeredSlides: true,
-  loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
