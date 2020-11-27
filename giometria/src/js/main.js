e20d0fa446bf2ada02f4444b0a6753397300dd78
marmamikonyan96 <58704193+marmamikonyan96@users.noreply.github.com>
2020-11-27 22:09:22 +0400

    github

--- giometria/src/js/main.js
+++ giometria/src/js/main.js
@@ -8,3 +8,8 @@ $(document).ready(function () {
     $(".overlay").removeClass("open");
   });
 });
+
+const prev = document.getElementById("btn-prev"),
+  prev = document.getElementById("btn-next"),
+  slides = document.querySelectorAll(".slide");
+slides.Wrapper = document.querySelectorAll(".slider-wrapper");
