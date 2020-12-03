5d3e7c38c96d053827aa15212169fba4e659179a
marmamikonyan96 <58704193+marmamikonyan96@users.noreply.github.com>
2020-12-04 00:06:11 +0400

    update

--- giometria/build/js/script.js
+++ giometria/build/js/script.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".open-menu").on("click",function(){$(".overlay").addClass("open")}),$(".close-menu").on("click",function(){$(".overlay").removeClass("open")})}),new Swiper(".swiper-container",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+$(document).ready(function(){$(".open-menu").on("click",function(){$(".overlay").addClass("open")}),$(".close-menu").on("click",function(){$(".overlay").removeClass("open")})});new Slider(document.getElementById("slider"));
--- giometria/src/js/main.js
+++ giometria/src/js/main.js
@@ -10,9 +10,31 @@ $(document).ready(function () {
   });
 });
 
-new Swiper(".swiper-container", {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+function slider(el) {
+  let self = this,
+    _slide = null,
+    _buttons = null;
+
+  this.el = el;
+
+  function init() {
+    _slide = self.el.getElementById("slide p");
+    _buttons = self.el.querySelector(".buttons");
+    self.currentSlide = 0;
+
+    _buttons.querySelector(".btn-left").addEventListener("click", function () {
+      if (self.currentSlide > 0) {
+        self.showSlide(self.currentSlide - 1);
+      }
+      self.showSlide(_slide.length - 1);
+    });
+
+    _buttons.querySelector(".btn-right").addEventListener("click", function () {
+      if (self.currentSlide < _slide.length - 1) {
+        self.showSlide(self.currentSlide + 1);
+      }
+      self.showSlide(0);
+    });
+  }
+}
+let slider = new Slider(document.getElementById("slider"));
