94d9ea2621c29f9b6912b84eb89751064cc0937d
marmamikonyan96 <58704193+marmamikonyan96@users.noreply.github.com>
2020-12-04 21:51:01 +0400

    update

---
+++ giometria/gulpfile.js
@@ -0,0 +1,83 @@
+/** @format */
+
+const gulp = require("gulp");
+const concat = require("gulp-concat");
+const imagemin = require("gulp-imagemin");
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+const uglify = require("gulp-uglify");
+const browserSync = require("browser-sync").create();
+const sourcemaps = require("gulp-sourcemaps");
+const sass = require("gulp-sass");
+const styleFiles = ["./src/sass/main.scss"];
+const scriptFiles = ["./src/js/main.js"];
+
+gulp.task("styles", () => {
+  return gulp
+    .src(styleFiles)
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+
+    .pipe(concat("style.css"))
+
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+        cascade: false,
+      })
+    )
+
+    .pipe(
+      cleanCSS({
+        level: 2,
+      })
+    )
+    .pipe(sourcemaps.write("./"))
+
+    .pipe(gulp.dest("./build/css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("img-compress", () => {
+  return gulp
+    .src("./src/img/**")
+    .pipe(
+      imagemin({
+        progresive: true,
+      })
+    )
+    .pipe(gulp.dest("./build/img/"));
+});
+
+gulp.task("scripts", () => {
+  return gulp
+    .src(scriptFiles)
+
+    .pipe(concat("script.js"))
+
+    .pipe(
+      uglify({
+        toplevel: true,
+      })
+    )
+    .pipe(gulp.dest("./build/js"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("watch", () => {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
+
+  gulp.watch("./src/sass/**/*.scss", gulp.series("styles"));
+  gulp.watch("./src/img/**", gulp.series("img-compress"));
+  gulp.watch("./src/js/**/*.js", gulp.series("scripts"));
+
+  gulp.watch("./*.html").on("change", browserSync.reload);
+});
+gulp.task(
+  "default",
+  gulp.series(gulp.parallel("styles", "scripts", "img-compress"), "watch")
+);
---
+++ giometria/src/js/dist/main.dev.js
@@ -0,0 +1,56 @@
+"use strict";
+
+/** @format */
+$(document).ready(function () {
+  $(".open-menu").on("click", function () {
+    $(".overlay").addClass("open");
+  });
+  $(".close-menu").on("click", function () {
+    $(".overlay").removeClass("open");
+  });
+});
+var $owl = $(".owl-carousel");
+$owl.children().each(function (index) {
+  $(this).attr("data-position", index); // NB: .attr() instead of .data()
+});
+$owl.owlCarousel({
+  items: 1,
+  nav: true,
+  dots: false,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true
+    },
+    600: {
+      items: 1,
+      nav: true
+    },
+    798: {
+      items: 1,
+      nav: true
+    },
+    831: {
+      items: 1,
+      nav: true
+    },
+    999: {
+      items: 1,
+      nav: true
+    },
+    1000: {
+      items: 1,
+      nav: true
+    },
+    1920: {
+      items: 1,
+      nav: true
+    }
+  }
+});
+$(document).on("click", ".owl-item>div", function () {
+  var $speed = 100; // in ms
+
+  $owl.trigger("to.owl.carousel", [$(this).data("position"), $speed]);
+});
