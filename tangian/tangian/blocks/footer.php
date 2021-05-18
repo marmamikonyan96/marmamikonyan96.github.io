c0ee6eb4255558dfc5dde1bad583eb0b114d0aa4
marmamikonyan96 <58704193+marmamikonyan96@users.noreply.github.com>
2021-05-18 10:12:33 +0400

    Add

---
+++ tangian/tangian/blocks/footer.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="am">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name = "viewport" content = "width = 320,
+       initial-scale = 2.3, user-scalable = no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta http-equiv="cleartype" content="on">
+    <meta http-equiv="msthemecompatible" content="no">
+
+    <title>Մելիք Թանգյան փաստաբանական գրասենյակ</title>
+    <link rel="stylesheet" href="css/main.css" />
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"/>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@1,500;1,600;1,700;1,800&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,400&display=swap" rel="stylesheet" />
+  </head>
+
+  <body>
+    <footer>
+        <div class="container">
+          <div class="about">
+            <ul>
+              <li><a href="about.php">Մեր մասին</a></li>
+              <li><a href="colegue.php">Մեր Հաճաճախորդներ</a></li>
+            </ul>
+            <div class="copyright">
+              <p>Copyright © 2011 - 2020 <span>MELIQ TANGYAN</span> 
+              </p>
+            </div>
+          </div>
+          <div class="adress">
+            <p>Հասցե՝ <span>Գարեգին Նժդեհ 64</span> <i class="fas fa-map-pin"></i></i> </p>
+            <p>հեռ: 093 8000 53 <span><i class="fas fa-phone-square"></i></span></p>
+
+            <div class="contact_us">
+              <a href="contact.php">Ուղիղ կապ մեզ հետ</a>
+            </div>
+          </div>
+          <div class="mapouter"><div class="gmap_canvas"><iframe width="400" height="200" id="gmap_canvas" src="https://maps.google.com/maps?q=48%2F2%20Garegin%20Nzhdeh%20Str%2C0026%20Yerevan%2C%20Armenia&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.whatismyip-address.com/nordvpn-coupon/"></a></div><style>.mapouter{position:relative;text-align:right;
+            height:400px;width:600px;}.gmap_canvas {overflow:hidden;background:none!important;height:200px;width:600px;}</style></div>
+        </div>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="js/main.js"></script>   
+ 
+  </body>
+</html>
